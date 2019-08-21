63d7b77f71ff15c2f644cfb98a1560a0d214f870
zalachenka <saliou.armony@gmail.com>
2019-08-21 12:21:58 +0200

    first commit, finished exo

---
+++ test.js
@@ -0,0 +1,24 @@
+const userInput = prompt("Saisissez votre âge:");
+userInput;
+"25"
+console.log(userInput);
+25
+
+alert("Warning...");
+
+alert("I know how to trigger an alert");
+
+const friends = "my friend";
+
+alert("Hello "+friends)
+
+const userAge = prompt("How old are you?")
+
+age = userAge
+"25"
+alert("Vous avez "+age+" ans")
+
+console.log("Vous avez "+age+" ans")
+
+console.log((3*4)-2)
+
